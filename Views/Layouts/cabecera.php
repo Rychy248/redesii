41e01dbb8ebce49a63747e487f5564ca94ea9fa8
Rychy <jorgeajrhh@gmail.com>
2021-09-10 15:52:09 -0600

    Borre el menu anterior

--- Views/Layouts/cabecera.php
+++ Views/Layouts/cabecera.php
@@ -3,30 +3,6 @@
   <h2>Redes de computadoras II</h2>
 </div>
 <div class="container">
-<<<<<<< HEAD
-  <ul>
-    <li class="nav-item"><a href="?controller=semana1&&action=contenido">Semana 1</a></li>
-    <li class="nav-item"><a href="?controller=semana2&&action=contenido">Semana 2</a></li>
-    <li class="nav-item"><a href="?controller=semana3&&action=contenido">Semana 3</a></li>
-    <li class="nav-item"><a href="?controller=semana4&&action=contenido">Semana 4</a></li>
-    <li class="nav-item"><a href="?controller=semana5&&action=contenido">Semana 5</a></li>
-    <li class="nav-item"><a href="?controller=semana6&&action=contenido">Semana 6</a></li>
-    <li class="nav-item"><a href="?controller=semana7&&action=contenido">Semana 8</a></li>
-    <li class="nav-item"><a href="?controller=semana8&&action=contenido">Semana 9</a></li>
-    <li class="nav-item"><a href="?controller=semana1&&action=underConstruction">About Us! :)</a></li>
-    <li class="nav-item"><a href="?controller=login&&action=index">Cerrar Sesion! :)</a></li>
-  </ul>
-=======
-<!-- <ul>
-  <li class="nav-item"><a href="?controller=semana3&&action=contenido">Semana 3</a></li>
-  <li class="nav-item"><a href="?controller=semana4&&action=contenido">Semana 4</a></li>
-  <li class="nav-item"><a href="?controller=semana5&&action=contenido">Semana 5</a></li>
-  <li class="nav-item"><a href="?controller=semana6&&action=contenido">Semana 6</a></li>
-  <li class="nav-item"><a href="?controller=semana7&&action=contenido">Semana 8</a></li>
-  <li class="nav-item"><a href="?controller=semana8&&action=contenido">Semana 9</a></li>
-  <li class="nav-item"><a href="?controller=semana1&&action=underConstruction">About Us! :)</a></li>
-  <li class="nav-item"><a href="?controller=login&&action=index">Cerrar Sesion! :)</a></li>
-</ul> -->
 <table>
         <tr>
           <th>
@@ -64,5 +40,4 @@
 
   </table>
 
->>>>>>> 2758368dd2db0afd27433290df13d782b2f2e8c2
 </div>
